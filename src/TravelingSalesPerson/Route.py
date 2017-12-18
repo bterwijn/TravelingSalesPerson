c7e2386982776bf20f81ae2e7fb9d0a60b3bccfe
Bas Terwijn <bterwijn@gmail.com>
2017-12-18 22:53:43 +0100

    saving and loading Route objects

--- src/TravelingSalesPerson/Route.py
+++ src/TravelingSalesPerson/Route.py
@@ -7,7 +7,7 @@ class Route:
     
     def __init__(self,nrCities,startCity):
         """initialize with the number of cities and the city the Route starts with"""
-        self.distance=0
+        self.distance=float(0)
         self.route=[startCity]
         self.remainingCities={x for x in range(0,nrCities)}
         self.remainingCities.remove(startCity)
@@ -56,10 +56,44 @@ class Route:
         self.remainingCities.add(last)
 
     def unselectLastCityHelper(self,myMap):
+        """helper function to unselect the last selected city on the route"""
         last=self.route.pop()
         c=self.getCurrentCity()
         self.distance-=myMap.getDistance(c,last)
 
+    def save(self,filename):
+        """save Map to file"""
+        with open(filename, "w") as f:
+            f.write("route: "+str(self.route)+"\n")
+            f.write("distance: "+str(self.distance)+"\n")
+            f.write("remainingCities: ")
+            if (len(self.remainingCities)==0):
+                f.write("{}\n") # otherwise it writes "set()"
+            else:
+                f.write(str(self.remainingCities)+"\n")
+
+    def load(self,filename):
+        """load Map from file"""
+        self.distance=0
+        self.route=[]
+        self.remainingCities=set()
+        with open(filename, "r") as f:
+            line=f.readline()
+            line=line[line.index("[")+1:]
+            line=line[:line.index("]")]
+            print(line)
+            for i in line.split(","):
+                if (len(i))>0:
+                    self.route.append(int(i))
+            self.distance=float(f.readline().split(" ")[1])
+            line=f.readline()
+            line=line[line.index("{")+1:]
+            line=line[:line.index("}")]
+            print(line)
+            for i in line.split(","):
+                if (len(i))>0:
+                    self.remainingCities.add(int(i))
+                
     def unitTest():
         """run unit tests"""
         myMap=Map()
@@ -68,13 +102,13 @@ class Route:
         route=myMap.initRoute()
         Route.recursiveTest(0,nrCities-1,myMap,route)
 
-    def recursiveTest(dept,maxDepth,myMap,route):
-        if dept<maxDepth:
+    def recursiveTest(depth,maxDepth,myMap,route):
+        if depth<maxDepth:
             dist1=route.getDistance()
             c=next(iter(route.getRemainingCities())) # select first element
             print(route)
             route.selectNextCity(myMap,c)
-            Route.recursiveTest(dept+1,maxDepth,myMap,route)
+            Route.recursiveTest(depth+1,maxDepth,myMap,route)
             route.unselectLastCity(myMap)
             dist2=route.getDistance()
             assert dist1-dist2<0.000001, "Error in recursive distance test"
