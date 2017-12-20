eb5382313bd73f2ecbd9241cc5b628267d657061
Bas Terwijn <bterwijn@gmail.com>
2017-12-20 01:02:31 +0100

    forgot to add imports.py

---
+++ src/imports.py
@@ -0,0 +1,11 @@
+
+# system
+from math import sqrt
+from random import uniform
+
+# my sources
+from TravelingSalesPerson.Position import *
+from TravelingSalesPerson.City import *
+from TravelingSalesPerson.Route import *
+from TravelingSalesPerson.Map import *
+
