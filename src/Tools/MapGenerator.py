11e65604953165d6adb77a8b1a714a271e617515
Bas Terwijn <bterwijn@gmail.com>
2017-12-22 15:09:45 +0100

    MapGenerator added

---
+++ src/Tools/MapGenerator.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append('..')
+from imports import *
+
+if len(sys.argv)<4 or sys.argv[1]=="-h" or sys.argv[1]=="--help":
+    print("Generates a map with a number of cities and saves it to file.")
+    print("The positions of cities have an arbitrary dimension")
+    print("A city's first coordinate is randomly choosen between 0 and c1")
+    print("        second coordinate is randomly choosen between 0 and c2")
+    print("        etc.")
+    print("  usage: "+sys.argv[0]+" <filename> <nrCities> <c1> <c2> ...")
+    print("  example: "+sys.argv[0]+" myMap.txt 20 10.0 10.0")
+    exit()
+
+filename=sys.argv[1]
+nrCities=int(sys.argv[2])
+coordinates=[float(i) for i in sys.argv[3:]]
+maxPosition=Position(coordinates)
+
+myMap=Map()
+myMap.randomizeMap(nrCities,maxPosition)
+myMap.save(filename)
